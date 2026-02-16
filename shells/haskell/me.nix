8a844aa9a247088354f5bf65251bce8f64bc89a0
zepzeper <wouterschiedam@hotmail.com>
2026-02-16 09:48:03 +0100

    nixos-rebuild:   22   2026-02-15 17:06:58   (current)

--- shells/haskell/me.nix
+++ shells/haskell/me.nix
@@ -1,5 +1,4 @@
-{ pkgs ? import <nixpkgs> {} }:
-
+{pkgs ? import <nixpkgs> {}}:
 pkgs.mkShell {
   buildInputs = [
     pkgs.haskell.compiler.ghc96
@@ -15,7 +14,7 @@ pkgs.mkShell {
     pkgs.pkg-config
     pkgs.glibcLocales
   ];
-  
+
   shellHook = ''
     export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
       pkgs.gmp
@@ -24,11 +23,11 @@ pkgs.mkShell {
       pkgs.zlib
       pkgs.openssl
     ]}:$LD_LIBRARY_PATH
-    
+
     export PATH="${pkgs.haskell.compiler.ghc96}/bin:$PATH"
-    
+
     export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig:${pkgs.zlib.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
-    
+
     export LANG=en_US.UTF-8
     export LC_ALL=en_US.UTF-8
     export LOCALE_ARCHIVE=${pkgs.glibcLocales}/lib/locale/locale-archive
