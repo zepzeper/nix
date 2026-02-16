2f971642d4ddbd400a7070299fb34d55353d8451
zepzeper <wouterschiedam@hotmail.com>
2026-02-16 21:36:09 +0100

    nixos-rebuild:   24   2026-02-16 17:50:25   (current)

--- nixos/modules/system/networking/default.nix
+++ nixos/modules/system/networking/default.nix
@@ -9,22 +9,18 @@
     settings = {
       # Listen on localhost only
       listen-address = "127.0.0.1";
-
       # Don't read /etc/resolv.conf
       no-resolv = true;
-
       # Set port to 0 for local caching only
       port = 0;
-
-      # Upstream DNS servers
+      # Upstream DNS servers - USE PI-HOLE FIRST
       server = [
+        "192.168.1.174" # Pi-hole
         "1.1.1.1"
         "8.8.8.8"
-        "8.8.4.4"
       ];
     };
   };
-
   networking.networkmanager = {
     enable = true;
     dns = "dnsmasq";
