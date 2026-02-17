7a92180bef00dc7ac37f6a2227a3d4aeee5d8ca5
zepzeper <wouterschiedam@hotmail.com>
2026-02-17 13:46:22 +0100

    Update dotfiles submodule

---
+++ home-manager/modules/platforms/nixos/cursor/default.nix
@@ -0,0 +1,13 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+home.pointerCursor = {
+  gtk.enable = true;
+  x11.enable = true;
+  package = pkgs.bibata-cursors;
+  name = "Bibata-Modern-Classic";
+  size = 24;
+};
+}
