bcdedb1f22f91c589bfd3a84c6730f406841dd95
zepzeper <wouterschiedam@hotmail.com>
2026-02-17 13:47:08 +0100

    nixos-rebuild:   24   2026-02-16 17:50:25   (current)

--- home-manager/modules/platforms/nixos/cursor/default.nix
+++ home-manager/modules/platforms/nixos/cursor/default.nix
@@ -3,11 +3,11 @@
   pkgs,
   ...
 }: {
-home.pointerCursor = {
-  gtk.enable = true;
-  x11.enable = true;
-  package = pkgs.bibata-cursors;
-  name = "Bibata-Modern-Classic";
-  size = 24;
-};
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 24;
+  };
 }
