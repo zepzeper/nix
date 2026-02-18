695c6aeda6b895d299287fb354ae9fdff9eec331
zepzeper <wouterschiedam@hotmail.com>
2026-02-18 10:17:32 +0100

    nixos-rebuild:   27   2026-02-18 09:50:38   (current)

--- nixos/modules/apps/default.nix
+++ nixos/modules/apps/default.nix
@@ -1,10 +1,8 @@
-_: {
-  imports = [
-    ./browser
-    ./creative
-    ./media
-    ./reader
-    ./steam
-    ./work
-  ];
+{lib, ...}: let
+  currentDir = ./.;
+  isDirectoryAndNotTemplate = name: type: type == "directory";
+  directories = lib.filterAttrs isDirectoryAndNotTemplate (builtins.readDir currentDir);
+  importDirectory = name: import (currentDir + "/${name}");
+in {
+  imports = lib.mapAttrsToList (name: _: importDirectory name) directories;
 }
