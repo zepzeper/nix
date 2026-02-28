178d50559c504daf3ccd815a397fa5f5447d50c6
zepzeper <wouterschiedam@hotmail.com>
2026-02-28 15:17:35 +0100

    Hostname

--- home-manager/modules/shell/zsh/default.nix
+++ home-manager/modules/shell/zsh/default.nix
@@ -2,25 +2,30 @@
   config,
   lib,
   pkgs,
+  role,
   ...
 }: let
   cfg = config.modules.shell;
+  
+  # Define prompt color based on role
+  promptColor = if role == "workstation" then "green" else "red";
 in {
   options.modules.shell = {
     zsh = lib.mkEnableOption "zsh configuration";
+    role = lib.mkOption {
+      type = lib.types.str;
+      default = "server";
+      description = "System role (workstation or server)";
+    };
   };
-
   config = lib.mkIf cfg.zsh {
     programs.zsh = {
       enable = true;
-
       syntaxHighlighting.enable = true;
       autosuggestion.enable = true;
-
       shellAliases = {
         cat = "bat --paging=never";
       };
-
       plugins = [
         {
           name = "vi-mode";
@@ -28,13 +33,12 @@ in {
           file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
         }
       ];
-
       initContent = ''
         bindkey -s '^f' "$HOME/.local/bin/tmux-sessionizer\n"
-
-        PROMPT="[%m] $PROMPT"
+        
+        # Add hostname to prompt with color
+        PROMPT="%F{${promptColor}}[%m]%f $PROMPT"
       '';
-
       oh-my-zsh = {
         enable = true;
         theme = "robbyrussell";
