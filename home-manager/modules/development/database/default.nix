e8799fc1bdfa6cb3970ffabcb40a0203c08de30f
zepzeper <wouterschiedam@hotmail.com>
2026-03-04 15:39:50 +0100

    nixos-rebuild:   54   2026-03-04 11:04:21   (current)

--- home-manager/modules/development/database/default.nix
+++ home-manager/modules/development/database/default.nix
@@ -10,13 +10,13 @@ in {
     database = lib.mkEnableOption "database tools";
   };
 
+  nixpkgs.config.permittedInsecurePackages = [
+    "beekeeper-studio-5.5.7"
+  ];
+
   config = lib.mkIf cfg.database {
     home.packages = with pkgs; [
       beekeeper-studio
-    ];
-
-    nixpkgs.config.permittedInsecurePackages = [
-      "beekeeper-studio-5.5.5"
     ];
   };
 }
